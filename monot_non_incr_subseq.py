9b37a3a9ecd585610343b1de18285f95563b89f2
melik-a <dzahelmelik@mail.ru>
2020-10-30 18:07:24 +0300

    added declaration of two func

--- monot_non_incr_subseq.py
+++ monot_non_incr_subseq.py
@@ -10,6 +10,12 @@ def len_of_max_non_incr_sub_seq(sequence):
             curr_sub_seq = 1
     return max_sub_seq
 
+def longest_non_incr_subseq():
+    pass
+
+def longest_incr_subseq():
+    pass
+
 if __name__ == "__main__":
     A = [7, 11, 9, 0, 11, 15, 0, 18, 2, 14, 16, 1 , 5, 12, 14, 0, 10, 11] 
     print("len of max non-increasing sub sequence", len_of_max_non_incr_sub_seq(A))
