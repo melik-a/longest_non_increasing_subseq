8a0edfeed920bf60edd2594cbdd2f8aceac6260a
melik-a <dzahelmelik@mail.ru>
2020-11-08 21:51:29 +0300

    some refactoring work/ adding comments

--- monot_subsequences.py
+++ monot_subsequences.py
@@ -1,3 +1,9 @@
+"""to do: 
+        1) provide finding all longest subsequences, not only first
+        2) cover with tests
+        3) add n * logarithmic algorithms for all subtasks
+"""
+
 #task 
 def max_non_incr_sub_seq(sequence):
     max_subseq_len = 1
@@ -38,7 +44,7 @@ def len_longest_incr_subseq_n2(sequence):
     sub_len = [1] * len(sequence)
     for i in range(len(sequence) - 1):
         for j in range(i + 1, len(sequence)):
-            if (sequence[i] <= sequence[j]) and (sub_len[j] <= sub_len[i]):
+            if (sequence[i] < sequence[j]) and (sub_len[j] <= sub_len[i]):
                 sub_len[j] = sub_len[i] + 1
     print(sub_len)
     return max(sub_len)
@@ -46,12 +52,12 @@ def len_longest_incr_subseq_n2(sequence):
 
 def longest_non_incr_subseq_n2(sequence):
     sequence_len = len(sequence)
-    pos = 0 
+    max_subseq_len = 0
+    pos  = 0 
     #length of the longest non increasing subsequence ending in the element with index i.
     sub_len = [1] * sequence_len
     trace = [-1] * sequence_len
-    max_subseq_len = 0
-    
+
     for i in range(sequence_len):
         for j in range(i):
             if (sequence[i] <= sequence[j]) and (sub_len[j] + 1 > sub_len[i]):
@@ -67,28 +73,40 @@ def longest_non_incr_subseq_n2(sequence):
         pos = trace[pos]
 
     path.reverse()
-    
-
-    """ path = []
-    flag = True
-    el = -1
-    for i in range(pos):
-        if (sequence[i] >= sequence[i + 1]) and el == -1:
-            path.append(sequence[i])
-            el = sequence[i + 1]
-            flag = False
-        elif el >= sequence[i + 1]:
-            path.append(el)
-            el = sequence[i + 1]
-    path.append(sequence[pos]) """ 
 
     print(f"sequence = {sequence}\nsub_len = {sub_len}\npos = {pos}")
     print(f"sub_seq = {path}")
+    
     return max(sub_len)
 
 
 def longest_incr_subseq_n2(sequence):
-    pass
+    sequence_len = len(sequence)
+    max_subseq_len = 0
+    pos = 0 
+    #length of the longest non increasing subsequence ending in the element with index i.
+    sub_len = [1] * sequence_len
+    trace = [-1] * sequence_len
+    
+    for i in range(sequence_len):
+        for j in range(i):
+            if (sequence[i] > sequence[j]) and (sub_len[j] + 1 > sub_len[i]):
+                sub_len[i] = sub_len[j] + 1
+                trace[i] = j
+        if sub_len[i] > max_subseq_len:
+            max_subseq_len = sub_len[i]
+            pos = i
+    
+    path = []
+    while pos != -1:
+        path.append(sequence[pos])
+        pos = trace[pos]
+    path.reverse()
+    
+    print(f"sequence = {sequence}\nsub_len = {sub_len}\npos = {pos}")
+    print(f"sub_seq = {path}")
+
+    return len(path)
 
 
 def len_longest_non_incr_subseq_log(sequence):
@@ -106,6 +124,16 @@ def test_max_non_incr_sub_seq():
         print(max_non_incr_sub_seq(i))
 
 def tests():
+    """ testing all task functions:
+            1) max_non_incr_sub_seq()
+            2) len_longest_non_incr_subseq_n2()
+            3) len_longest_incrt_subseq_n2()
+            4) longest_non_incr_subseq_n2()
+            5) longest_incr_subseq_n2()
+            6) longest_non_incr_subseq_nlogn()
+            7) longest_incr_subseq_nlogn()
+            8) ...
+    """
     pass
 
 
@@ -117,7 +145,7 @@ def main():
     E = [11, 17, 15, 9, 7, 19, 14, 10, 19, 20, 21, 0]
     tests = [A, B, C, D, E]
     for i in tests:
-        print(longest_non_incr_subseq_n2(i))
+        print(longest_incr_subseq_n2(i))
         print("-------------------------------------------------------")
 
 
