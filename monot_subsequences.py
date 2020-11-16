5b3db03a8115fd9da4bb9395a9fd4497b9651bc1
melik-a <dzahelmelik@mail.ru>
2020-11-16 19:26:05 +0300

    ьÑmaybe this is the final version. main functionality оÐjust added

--- monot_subsequences.py
+++ monot_subsequences.py
@@ -1,7 +1,5 @@
-"""to do: 
-        1) provide finding all longest subsequences, not only first
-        2) cover with tests
-        3) add n * logarithmic algorithms for all subtasks
+""" to do: 
+         1) add functionality to generate random sequences to find LNIS, LIS, LDS, LNDS
 """
 
 
@@ -204,16 +202,28 @@ def longest_non_decr_subseq_nlogn(sequence):
     return subseq[::-1]
 
 
-def main():
-    A = [7, 11, 9, 0, 11, 15, 0, 18, 2, 14, 16, 1, 5, 12, 14, 0, 10, 11]
-    B = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
-    C = [7, 11, 9, 7, 11, 15, 5, 18, 2, 14, 16, 1, 5, 12, 14, 0, 10, 11, 0]
-    D = [0, 0, 0, 0, 0, 0]
-    E = [11, 17, 15, 9, 7, 19, 14, 10, 19, 20, 21, 0]
-    test_set = [A, B, C, D, E]
-    for i in test_set:
-        print (f"test_seq = {i}\n")
-        print(f"-------------------------------------------------------------------")
+def find(sequence = None):
+    if sequence == None:
+        A = [7, 11, 9, 0, 11, 15, 0, 18, 2, 14, 16, 1, 5, 12, 14, 0, 10, 11]
+        B = [0, 8, 4, 12, 2, 10, 6, 14, 1, 9, 5, 13, 3, 11, 7, 15]
+        C = [7, 11, 9, 7, 11, 15, 5, 18, 2, 14, 16, 1, 5, 12, 14, 0, 10, 11, 0]
+        D = [0, 0, 0, 0, 0, 0]
+        E = [11, 17, 15, 9, 7, 19, 14, 10, 19, 20, 21, 0]
+        demo_set = [A, B, C, D, E]
+
+        print(f"demo_sequences:")
+        print(f"\t\tA = {A}")
+        print(f"\t\tB = {B}")
+        print(f"\t\tC = {C}")
+        print(f"\t\tD = {D}")
+        print(f"\t\tE = {E}")
+    else:
+        demo_set = []
+        demo_set.append(sequence)
+
+    for i in demo_set:
+        print(f"\nseq = {i}\n")
+        print(f"-------------------------------------------------------------------\n")
         
         print(f"len_longest_non_incr_subseq_n2 = {len_longest_non_incr_subseq_n2(i)}\n")
         
@@ -232,12 +242,61 @@ def main():
         print(f"###################################################################")
         print(f"###################################################################")
         print(f"###################################################################\n")
-    
-    print(f"A = {A}")
-    print(f"B = {B}")
-    print(f"C = {C}")
-    print(f"D = {D}")
-    print(f"E = {E}\n")
+
+
+def main():
+    """ 
+        comment
+    """
+    import random
+    print("/////////////////////////////////////////////////////////////////")
+    print("programm to find all subsequences (LNIS, LIS, LDS, LNDS) in a sequence(integer)")
+    print("/////////////////////////////////////////////////////////////////")
+    while True:
+        print(f'''select the task:
+                        1) enter a sequence.
+                        2) generate a random sequence.
+                        3) demonstration.
+                        0) exit.''')
+        
+        try:
+            choice = int(input("enter the task number: "))
+        except:
+            print("task number must be one of the available values. please try again.\n")
+            continue
+        
+        if choice == 0:
+            break
+        elif choice == 3:
+            find()
+        elif choice == 1:
+            while True:
+                try:
+                    size = int(input("enter the size of the sequence: "))
+                    if size <= 0:
+                        raise Exception
+                except:
+                    print("please enter the correct size number.\n")
+                    continue
+                break
+            print("\nenter the sequence: ")
+            seq = []
+            for i in range(size):
+                while True:
+                    try: el = int(input(f"enter the {i} element = "))
+                    except: print("element must be integer. please enter the correct element value\n")
+                    seq.append(el)
+                    print(f"current sequence = {seq}\n")
+                    break
+            print(f"your sequence = {seq}")
+            find(seq)
+
+        elif choice == 2:
+            size = random.randrange(3, 50)
+            seq = [random.randrange(0, 200) for i in range(size)]
+            find(seq)
+
+    print(f"bye, bye")
 
 
 if __name__ == "__main__":
